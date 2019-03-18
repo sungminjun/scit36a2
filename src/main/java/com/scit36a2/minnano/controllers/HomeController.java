c7ec7cdef9e5643a9efd68b37363cff9d012584b
Lee YeongChang <clvh753@naver.com>
2019-03-18 11:25:48 +0900

    HomeController에 lyc_main추가 및 'lyc_main.jsp'파일 추가입니다

--- src/main/java/com/scit36a2/minnano/controllers/HomeController.java
+++ src/main/java/com/scit36a2/minnano/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// 홈으로 이동
+// �솃�쑝濡� �씠�룞
 @Controller
 public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -15,6 +15,13 @@ public class HomeController {
 	public String home() {
 		logger.info("welcome home.");
 		return "devhome";
+	}
+	
+	@RequestMapping(value = "/lyc_main", method = RequestMethod.GET)
+	public String lyc_main() {
+		logger.info("welcome home.");
+		
+		return "lyc_main";
 	}
 
 }

4127482237e0ec0f43ac6190e06f4412d2ec5718
igorh <igorh@DESKTOP-TLLVOMS>
2019-03-20 09:04:08 +0900

    DB sample data 오류수정, HomeController 인코딩 깨짐 복구

--- src/main/java/com/scit36a2/minnano/controllers/HomeController.java
+++ src/main/java/com/scit36a2/minnano/controllers/HomeController.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// �솃�쑝濡� �씠�룞
+// 홈으로 이동
 @Controller
 public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		logger.info("welcome home.");

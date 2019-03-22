749aa87e683d830c1aebcb666f14cae5b759e1c3
kimnemo <madamnurse43@gmail.com>
2019-03-22 18:50:58 +0900

    로그인,멤버&회사 등록-수정(아직 아이디,비밀번호 찾기 부분은 구현하지 못함)

--- src/main/java/com/scit36a2/minnano/dao/MemberDAO.java
+++ src/main/java/com/scit36a2/minnano/dao/MemberDAO.java
@@ -12,14 +12,18 @@ public interface MemberDAO {
 	//로그인
 	public Employee selectOne(Employee employee);
 	//현재 등록된 전체 직원 목록 불러오기
-	public List<Employee> selectAll(Employee employee);
-	//사장님 정보수정 처리
-	public int updateOwner(Employee employee);
+	public List<Employee> selectMemberAll(Employee employee);
+	//사장님,멤버 정보수정 처리
+	public int updateMember(Employee employee);
 	// 회원가입전 회사 seq 받아오기
 	public int reqCompSeq();
 	// 회원가입
 	public int join(HashMap<String, Object> map);
-	//사업자등록번호 중복확인
+	//사업자등록번호 중복확인,회사 정보 받아오기
 	public Company selectCompanyOne(Company company);
+	//회사 정보 수정 처리
+	public int updateCompany(Company company);
+	//멤버 등록 처리
+	public int joinMember(Employee employee);
 	
 }

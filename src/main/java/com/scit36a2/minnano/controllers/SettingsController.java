f2b358f8e3bee95887244c8e76f15f72f6d6473d
sungminjun <46203992+sungminjun@users.noreply.github.com>
2019-03-25 10:14:51 +0900

    Update SettingsController.java

--- src/main/java/com/scit36a2/minnano/controllers/SettingsController.java
+++ src/main/java/com/scit36a2/minnano/controllers/SettingsController.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,189 +22,240 @@ import com.scit36a2.minnano.vo.Seat;
 // 관리페이지 기능 중 회원관련 기능을 제외한, 테이블 등록/관리, 메뉴 등록/관리, 지출 등록/관리
 @Controller
 public class SettingsController {
+
 	@Autowired
 	SalesRepo repo;
 
 	private static final Logger logger = LoggerFactory.getLogger(SettingsController.class);
 
-	@RequestMapping(value = "choiTest", method = RequestMethod.GET)
-	public String insertseat(HttpSession session) {
-		System.out.println((Integer) session.getAttribute("comp_seq"));
-		return "choiTest";
+	@RequestMapping(value = "mgr", method = RequestMethod.GET)
+	public String mgr(HttpSession session) {
+		logger.info("welcome home");
+		return "mgr/mgr";
 	}
 
+	// page move method above.
+	//
+	///////
+	///////
+	//
+	// seat(table) manager methods below
+
+	/**
+	 * 해당 회사의 테이블 추가
+	 *
+	 * @author 최철규
+	 */
 	@RequestMapping(value = "addtable", method = RequestMethod.POST)
 	@ResponseBody
-	public String insertseat(String seat_id, HttpSession session) {
-
+	public String insertseat(Seat seat, HttpSession session) {
 		// int emp_auth_level = employee.getEmp_auth_level();
 		int comp_seq = (Integer) session.getAttribute("comp_seq");
-		
-		Seat seat = new Seat();
 		seat.setComp_seq(comp_seq);
-		seat.setSeat_id(seat_id);
+		System.out.println(seat);
 		// if (emp_auth_level == 9) {
 		int result = repo.insertseat(seat);
-		if (result == 1) {
-			System.out.println(seat);
+
+		if (result == 1)
 			return "success";
-		} else {
+		else
 			return "fail";
-		}
 	}
 
-	
-	@RequestMapping(value = "selectseat", method = RequestMethod.GET)
+	/**
+	 * 해당 회사의 전체 테이블 목록 조회
+	 * 
+	 * @author 최철규
+	 */
+	@RequestMapping(value = "selectseat", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Seat> selectseat(HttpSession session) {
 		int comp_seq = (Integer) session.getAttribute("comp_seq");
-		List<Seat> seatList = new ArrayList<Seat>(); 
+		List<Seat> seatList = new ArrayList<Seat>();
 		seatList = repo.selectseat(comp_seq);
-		System.out.println("seatList : " + seatList);
-		//List<Seat> re = (List)session.getAttribute("seat");
-		
 		return seatList;
 	}
 
-	// 테이블삭제(테이블이름입력받아서)
+	/**
+	 * 테이블삭제(테이블이름입력받아서)
+	 * 
+	 * @author 최철규
+	 */
 	@RequestMapping(value = "deleteseat", method = RequestMethod.POST)
 	@ResponseBody
-	public String deleteseat(int seat_seq,HttpSession session) {
-		int comp_seq = (Integer)session.getAttribute("comp_seq");
-		Seat seat = new Seat();
+	public String deleteseat(Seat seat, HttpSession session) {
+		// 수정사항) 테이블 이름이 아닌, 테이블 seq를 받아서 삭제하도록 변경
+		int comp_seq = (Integer) session.getAttribute("comp_seq");
 		seat.setComp_seq(comp_seq);
-		seat.setSeat_seq(seat_seq);
 		int result = repo.deleteseat(seat);
-		if (result==1) {
-			System.out.println("O");
-		}	else	{
-			System.out.println("X");
-		}
-		
-		return "success";
-
+		if (result == 1)
+			return "success";
+		else
+			return "fail";
 	}
 
-	// 테이블수정
+	/**
+	 * 테이블수정
+	 * 
+	 * @author 최철규
+	 */
 	@RequestMapping(value = "updateseat", method = RequestMethod.POST)
 	@ResponseBody
-	public String updateseat(HttpSession session,int seat_seq) {
-		int comp_seq = (Integer)session.getAttribute("comp_seq");
-		Seat seat = new Seat();
+	public String updateseat(HttpSession session, Seat seat) {
+		int comp_seq = (Integer) session.getAttribute("comp_seq");
 		seat.setComp_seq(comp_seq);
-		seat.setSeat_seq(seat_seq);
 		int result = repo.updateseat(seat);
+		if (result == 1)
 			return "success";
+		else
+			return "fail";
 	}
 
-	// 메뉴화면이동
-	@RequestMapping(value = "choitestmenu", method = RequestMethod.GET)
-	public String choitestmenu() {
-		return "choitestmenu";
-	}
+	// seat(table) manager methods above
+	//
+	///////
+	///////
+	//
+	// menu manager methods below
 
 	// 메뉴등록
-	@RequestMapping(value="insertMenu", method=RequestMethod.POST)
+	/**
+	 * 해당 점포의 메뉴를 등록
+	 * 
+	 * @author 최철규
+	 */
+	@RequestMapping(value = "insertMenu", method = RequestMethod.POST)
 	@ResponseBody
-	public String insertMenu(HttpSession session,Menu menu) {			//int menu_sellFlag,String menu_name,int menu_price,String menu_category
-		int comp_seq = (Integer)session.getAttribute("comp_seq");
-		//Menu menu = new Menu();
-		System.out.println("컨트롤러 comp_seq : " + comp_seq);
+	public String insertMenu(Menu menu, HttpSession session) {
+//		수정사항 개별 var를 string으로 받던것을 vo로 묶어서 받도록 변경
+		int comp_seq = (Integer) session.getAttribute("comp_seq");
+		System.out.println(menu);
 		menu.setComp_seq(comp_seq);
 		int result = repo.insertMenu(menu);
 		System.out.println("컨트롤러 result : " + result);
-	
-		return "success";
-	
+
+		if (result == 1)
+			return "success";
+		else
+			return "fail";
 	}
 
-	@RequestMapping(value = "selectMenu", method = RequestMethod.GET)
+	/**
+	 * 해당 점포의 메뉴 목록을 조회
+	 * 
+	 * @author 최철규
+	 */
+	@RequestMapping(value = "selectMenu", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Menu> selectMenu(HttpSession session) {
 		int comp_seq = (Integer) session.getAttribute("comp_seq");
-		List<Menu> menuList = new ArrayList<Menu>(); 
+		List<Menu> menuList = new ArrayList<Menu>();
 		menuList = repo.selectMenu(comp_seq);
 		System.out.println("menuList : " + menuList);
 		return menuList;
 	}
-	
+
+
+	/**
+	 * 메뉴 삭제
+	 * 
+	 * @author 최철규
+	 */
 	@RequestMapping(value = "deleteMenu", method = RequestMethod.POST)
 	@ResponseBody
-	public String deleteMenu(int menu_seq, HttpSession session) {
-		Menu menu = new Menu();
-		int comp_seq = (Integer)session.getAttribute("comp_seq");
+	public String deleteMenu(Menu menu, HttpSession session) {
+		int comp_seq = (Integer) session.getAttribute("comp_seq");
 		menu.setComp_seq(comp_seq);
-		menu.setMenu_seq(menu_seq);
 		int result = repo.deleteMenu(menu);
-		
-		return "success";
+
+		if (result == 1)
+			return "success";
+		else
+			return "fail";
 	}
 
+	/**
+	 * 메뉴 업데이트
+	 * 
+	 * @author 최철규, 전성민
+	 */
 	@RequestMapping(value = "updateMenu", method = RequestMethod.POST)
 	@ResponseBody
-	public String updateMenu(HttpSession session, int menu_seq, Employee employee) {
-		session.getAttribute("");
-		
-		/*	int Emp_auth_level = employee.getEmp_auth_level();
-		if (Emp_auth_level == 9) {
-			int result = repo.updateMenu(menu_seq);
+	public String updateMenu(HttpSession session, Menu menu) {
+		int comp_seq = (Integer) session.getAttribute("comp_seq");
+		menu.setComp_seq(comp_seq);
+		System.out.println(menu);
+		System.out.println(menu.getMenu_name());
+		System.out.println(menu.getMenu_name().getClass());
+ 
+		int result = repo.updateMenu(menu);
+		/*
+		 * int Emp_auth_level = employee.getEmp_auth_level(); if (Emp_auth_level == 9) {
+		 * int result = repo.updateMenu(menu_seq); return "success"; } else { return
+		 * "false"; }
+		 */
+		if (result == 1)
 			return "success";
-		} else {
-			return "false";
-		}*/
-		return null;
+		else
+			return "fail";
 	}
 
-	// 지출화면이동
-	@RequestMapping(value = "choitestexpense", method = RequestMethod.GET)
-	public String insertExpense() {
+	// menu manager methods above
+	//
+	///////
+	///////
+	//
+	// expense manager methods below
 
-		return "choitestexpense";// 지출화면 jsp 확인
-	}
-
-	@RequestMapping(value = "inexpense", method = RequestMethod.POST)
-	@ResponseBody
-	public String insertExpense(HttpSession session, Expense expense) {
-		int comp_seq = (Integer)session.getAttribute("comp_seq");
+	@RequestMapping(value = "insertExpense", method = RequestMethod.POST)
+	public @ResponseBody String insertExpense(HttpSession session, Expense expense) {
+		int comp_seq = (Integer) session.getAttribute("comp_seq");
+		System.out.println(expense);
 		expense.setComp_seq(comp_seq);
 		int result = repo.insertExpense(expense);
 		
-		return "success";
-	}
-
-	@RequestMapping(value = "selexpense", method = RequestMethod.GET)
-	@ResponseBody
-	public List<Expense> selectExpense(HttpSession session) {
-		int comp_seq = (Integer)session.getAttribute("comp_seq");
-		List<Expense> expenseList = new ArrayList<Expense>();
-		expenseList = repo.selectExpense(comp_seq);
-//		System.out.println("지출 목록 리스트expenseList : " + expenseList);
-		return expenseList;
-		
-	}
-
-	@RequestMapping(value = "delexpense", method = RequestMethod.POST)
-	@ResponseBody
-	public String deleteExpense(HttpSession session,int expense_seq) {
-		Expense expense = new Expense();
-		int comp_seq = (Integer)session.getAttribute("comp_seq");
-		expense.setComp_seq(comp_seq);
-		expense.setExpense_seq(expense_seq);
-		int result = repo.deleteExpense(expense);
+		if (result == 1)
 			return "success";
+		else
+			return "fail";
+	}
+
+	@RequestMapping(value = "selectExpense", method = RequestMethod.POST)
+	public @ResponseBody List<Expense> selectExpense(HttpSession session) {
+		int comp_seq = (Integer) session.getAttribute("comp_seq");
+		List<Expense> expenseList = repo.selectExpense(comp_seq);
+		return expenseList;
+	}
+
+	@RequestMapping(value = "deleteExpense", method = RequestMethod.POST)
+	@ResponseBody
+	public String deleteExpense(int expense_seq) {
+		int result = repo.deleteExpense(expense_seq);
+		if (result == 1)
+			return "success";
+		else
+			return "fail";
 	}
 
 	@RequestMapping(value = "updateExpense", method = RequestMethod.POST)
 	@ResponseBody
-	public String updateExpense(HttpSession session,Expense expense) {
-		
-		int comp_seq = (Integer)session.getAttribute("comp_seq");
-		expense.setComp_seq(comp_seq);
-		//expense.setExpense_seq(comp_seq);
-		System.out.println(expense);
-//		int result = repo.updateExpense(expense);
-		repo.updateExpense(expense);
-		return "success";
+	public String updateExpense(int expense_seq) {
+		int result = repo.updateExpense(expense_seq);
+		if (result == 1)
+			return "success";
+		else
+			return "fail";
 	}
+
+	// expense manager methods above
+	//
+	///////
+
+	// 삭제예정 -- mgr으로 통합
+	// 지출화면이동
+//	@RequestMapping(value = "insertExpense", method = RequestMethod.GET)
+//	public String insertExpense() {
+//		return "Expense";// 지출화면 jsp 확인
+//	}
 
 }
